1bbc33e773ca2f7ddb03e0f70bd905eb83f44122
Ranjith K <ranjith.masthikatte@gmail.com>
2021-04-05 03:25:57 +0200

    Adds the sanitizes to exclude from the analyzing

--- de.fraunhofer.iem.secucheck.analysis/src/main/java/de/fraunhofer/iem/secucheck/analysis/TaintAnalysis/SingleFlowTaintAnalysis/BoomerangSolver/guided/CustomDataFlowScope.java
+++ de.fraunhofer.iem.secucheck.analysis/src/main/java/de/fraunhofer/iem/secucheck/analysis/TaintAnalysis/SingleFlowTaintAnalysis/BoomerangSolver/guided/CustomDataFlowScope.java
@@ -5,12 +5,27 @@ import boomerang.scene.DeclaredMethod;
 import boomerang.scene.Method;
 import boomerang.scene.jimple.JimpleDeclaredMethod;
 import boomerang.scene.jimple.JimpleMethod;
+import de.fraunhofer.iem.secucheck.analysis.TaintAnalysis.SingleFlowTaintAnalysis.BoomerangSolver.Utility;
+import de.fraunhofer.iem.secucheck.analysis.query.MethodImpl;
 import soot.SootClass;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomDataFlowScope implements DataFlowScope {
+    List<MethodImpl> sanitizers = new ArrayList<>();
+
+    public CustomDataFlowScope(List<MethodImpl> sanitizers) {
+        this.sanitizers.addAll(sanitizers);
+    }
     @Override
     public boolean isExcluded(DeclaredMethod method) {
         JimpleDeclaredMethod m = (JimpleDeclaredMethod) method;
+
+        for (MethodImpl sanitizer : sanitizers) {
+            if (method.getSignature().equals(Utility.wrapInAngularBrackets(sanitizer.getSignature())))
+                return true;
+        }
 
         if (method.getSignature().equals("<org.owasp.webgoat.sql_injection.introduction.SqlInjectionLesson2: java.lang.String sanitize(java.lang.String)>")) {
             return true;
@@ -34,6 +49,16 @@ public class CustomDataFlowScope implements DataFlowScope {
 
     public boolean isExcluded(Method method) {
         JimpleMethod m = (JimpleMethod) method;
+
+        for (MethodImpl sanitizer : sanitizers) {
+            String[] arr = Utility.wrapInAngularBrackets(sanitizer.getSignature()).split(" ");
+            String subSignature = arr[arr.length - 2] + " " + arr[arr.length - 1];
+            subSignature = subSignature.replace(">", "");
+
+            System.out.println("SubSignature = " + subSignature);
+            if (m.getSubSignature().equals(subSignature))
+                return true;
+        }
 
         if (m.getSubSignature().equals("java.lang.String sanitize(java.lang.String)")) {
             return true;
