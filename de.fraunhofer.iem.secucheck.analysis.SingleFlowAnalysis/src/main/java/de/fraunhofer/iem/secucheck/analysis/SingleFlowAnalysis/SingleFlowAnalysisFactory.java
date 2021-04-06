611f54575e6cd43967f7d9e5d5987dc111067352
Ranjith K <ranjith.masthikatte@gmail.com>
2021-04-07 00:29:51 +0200

    Update SingleFlowAnalysisFactory.java

--- de.fraunhofer.iem.secucheck.analysis.SingleFlowAnalysis/src/main/java/de/fraunhofer/iem/secucheck/analysis/SingleFlowAnalysis/SingleFlowAnalysisFactory.java
+++ de.fraunhofer.iem.secucheck.analysis.SingleFlowAnalysis/src/main/java/de/fraunhofer/iem/secucheck/analysis/SingleFlowAnalysis/SingleFlowAnalysisFactory.java
@@ -6,7 +6,5 @@ import de.fraunhofer.iem.secucheck.analysis.query.TaintFlowImpl;
  * Factory to create respective solver's single Taint flow analysis. Currently Boomerang 3 and Flowdroid solvers are available.
  */
 public interface SingleFlowAnalysisFactory {
-
     SingleFlowAnalysis create(TaintFlowImpl flowQuery);
-
 }
