44981580ce8538b3bff429a882cfa2abdce9eb94
Enri Ozuni <enriozuni@hotmail.com>
2021-08-05 18:02:36 +0200

    Add changes to signature parser for entry points

--- de.fraunhofer.iem.secucheck.analysis.parser/src/main/java/de/fraunhofer/iem/secucheck/analysis/parser/methodsignature/SignatureParser.java
+++ de.fraunhofer.iem.secucheck.analysis.parser/src/main/java/de/fraunhofer/iem/secucheck/analysis/parser/methodsignature/SignatureParser.java
@@ -10,7 +10,7 @@ public class SignatureParser {
 	public static boolean matches(Object sootSignature, Object dslSignature) {
 		
 		ParsedMethodSignature parsedSootSignature = parseSootSignature(sootSignature);
-		ParsedMethodSignature parsedDSLSignature = parseDSLSignature(dslSignature);
+		ParsedMethodSignature parsedDSLSignature = parseDSLMethodSignature(dslSignature);
 		
 		if( parsedDSLSignature.getMethodArguments().size()==1 && 
 				parsedDSLSignature.getMethodArguments().get(0).equals("ANY") ) {
@@ -46,60 +46,70 @@ public class SignatureParser {
 	}
 	
 	
-	private static ParsedMethodSignature parseDSLSignature(Object signature) {
-		
-		String strSignature = signature.toString();
+	public static ParsedMethodSignature parseDSLMethodSignature(Object signature) {
 		ParsedMethodSignature parsedSignature = new ParsedMethodSignature();
-		String methodFullyQualifiedName;
+		String strSignature = signature.toString();
 		
-		if(StringUtils.isBlank(strSignature)) {
-			return parsedSignature;
-		}
-		
-		if(strSignature.contains(":")) {
-			String[] subSignatures = strSignature.split(":");
-			if(subSignatures.length == 2) {
-				parsedSignature.setClassName(subSignatures[0].replace("\\s+", ""));
-				methodFullyQualifiedName = subSignatures[1].trim();
+		try {
+			String methodPartialQualifiedName = "";
+			
+			if(StringUtils.isBlank(strSignature)) {
+				return parsedSignature;
+			}
+			
+			if(strSignature.contains(":")) {
+				String[] subSignatures = strSignature.split(":");
+				if(subSignatures.length == 2) {
+					parsedSignature.setClassName(subSignatures[0].replace("\\s+", ""));
+					methodPartialQualifiedName = subSignatures[1].trim();
+				} else {
+					return parsedSignature;
+				}
 			}
 			else {
-				throw new RuntimeException("Error during method signature parsing process.");
+				methodPartialQualifiedName = strSignature;
 			}
-		}
-		else {
-			parsedSignature.setClassName("");
-			methodFullyQualifiedName = strSignature;
-		}
-		
-		String[] splittedMethodFullyQualifiedName = methodFullyQualifiedName.split("\\s+", 2);
-		parsedSignature.setReturnType(splittedMethodFullyQualifiedName[0].trim());
-		
-		String methodNameAndParam = splittedMethodFullyQualifiedName[1].trim();
-		parsedSignature.setMethodName(methodNameAndParam.substring(0, methodNameAndParam.indexOf("(")));
-		
-		String methodParam = methodNameAndParam.substring(methodNameAndParam.indexOf("(")+1, methodNameAndParam.indexOf(")"));
-		List<String> methodArgs = new ArrayList<>();
-		if(StringUtils.isBlank(methodParam)){
-			methodArgs.add("");
-			parsedSignature.setMethodArguments(methodArgs);
-		} 
-		else {
-			String[] methodParamArray = methodParam.split(",");
-			for(String param : methodParamArray) {
-				methodArgs.add(param.replaceAll("\\s",""));
+			
+			String[] splittedMethodFullyQualifiedName = methodPartialQualifiedName.split("\\s+", 2);
+			parsedSignature.setReturnType(splittedMethodFullyQualifiedName[0].trim());
+			
+			String methodNameAndParam = splittedMethodFullyQualifiedName[1].trim();
+			parsedSignature.setMethodName(methodNameAndParam.substring(0, methodNameAndParam.indexOf("(")).trim());
+			
+			String methodParam = methodNameAndParam.substring(methodNameAndParam.indexOf("(")+1, methodNameAndParam.indexOf(")"));
+			List<String> methodArgs = new ArrayList<>();
+			if(StringUtils.isBlank(methodParam)){
+				methodArgs.add("");
+				parsedSignature.setMethodArguments(methodArgs);
+			} 
+			else {
+				String[] methodParamArray = methodParam.split(",");
+				for(String param : methodParamArray) {
+					methodArgs.add(param.replaceAll("\\s",""));
+				}
+				parsedSignature.setMethodArguments(methodArgs);
 			}
-			parsedSignature.setMethodArguments(methodArgs);
+		} catch(Exception e) {
+			throw new RuntimeException("Unable to parse method signature:\n"+strSignature);
 		}
 		
 		return parsedSignature;
 		
 	}
 	
+	public static String parseDSLClassOrPackageSignature(Object signature) {
+		String strSignature = signature.toString();
+		if(StringUtils.isBlank(strSignature)) {
+			return "";
+		}
+		return strSignature.replaceAll("\\s", "");
+	}
+	
 	
 	private static ParsedMethodSignature parseSootSignature(Object signature) {
 		
 		String strSignature = signature.toString();
-		return parseDSLSignature(strSignature.substring(1, strSignature.length() - 1));
+		return parseDSLMethodSignature(strSignature.substring(1, strSignature.length() - 1));
 		
 	}
 	
