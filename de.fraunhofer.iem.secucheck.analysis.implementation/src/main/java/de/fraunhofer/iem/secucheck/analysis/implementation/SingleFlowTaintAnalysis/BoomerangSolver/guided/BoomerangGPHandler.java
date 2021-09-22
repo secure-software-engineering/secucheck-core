89b6d4363689629e06db79f94dbc9671c074a082
ranjithk <ranjith.masthikatte@gmail.com>
2021-09-22 18:01:12 +0200

    Handles the default value in Kotlin's constructor, top level function and class level function

--- de.fraunhofer.iem.secucheck.analysis.implementation/src/main/java/de/fraunhofer/iem/secucheck/analysis/implementation/SingleFlowTaintAnalysis/BoomerangSolver/guided/BoomerangGPHandler.java
+++ de.fraunhofer.iem.secucheck.analysis.implementation/src/main/java/de/fraunhofer/iem/secucheck/analysis/implementation/SingleFlowTaintAnalysis/BoomerangSolver/guided/BoomerangGPHandler.java
@@ -11,11 +11,14 @@ import boomerang.scene.Val;
 import de.fraunhofer.iem.secucheck.analysis.configuration.SecucheckAnalysisConfiguration;
 import de.fraunhofer.iem.secucheck.analysis.datastructures.DifferentTypedPair;
 import de.fraunhofer.iem.secucheck.analysis.implementation.SingleFlowTaintAnalysis.BoomerangSolver.Utility;
-import de.fraunhofer.iem.secucheck.analysis.implementation.SingleFlowTaintAnalysis.datastructure.BoomerangTaintFlowPath;
 import de.fraunhofer.iem.secucheck.analysis.implementation.SingleFlowTaintAnalysis.TaintFlowPathUtility;
+import de.fraunhofer.iem.secucheck.analysis.implementation.SingleFlowTaintAnalysis.datastructure.BoomerangTaintFlowPath;
 import de.fraunhofer.iem.secucheck.analysis.query.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * This is the Secucheck DemandDriven Manager for Boomerang
@@ -64,6 +67,41 @@ public class BoomerangGPHandler implements IDemandDrivenGuidedManager {
     }
 
     /**
+     * Returns the backward query for the given Method and the statement
+     *
+     * @param sinkMethod   Sink Method from the specifications
+     * @param statement    Current Statement
+     * @param dataFlowEdge Dataflow edge
+     * @param dataFlowVal  Fact: dataFlowVal
+     * @return Backward query for given sink method
+     */
+    private BackwardQuery getBackWardQueriesBasedOnRules(Method sinkMethod, Statement statement, ControlFlowGraph.Edge dataFlowEdge, Val dataFlowVal) {
+        //For sinks there is always a InFlow, there is no OutFlow.
+
+        // Check for the InFlow parameters
+        if (sinkMethod.getInputParameters() != null) {
+            for (InputParameter input : sinkMethod.getInputParameters()) {
+                int parameterIndex = input.getParamID();
+                if (statement.getInvokeExpr().getArgs().size() >= parameterIndex) {
+                    if (statement.getInvokeExpr().getArg(parameterIndex).toString().equals(dataFlowVal.toString())) {
+                        return BackwardQuery.make(dataFlowEdge, statement.getInvokeExpr().getArg(parameterIndex));
+                    }
+                }
+            }
+        }
+
+        // Check for InFlow this-object
+        if (sinkMethod.isInputThis() &&
+                statement.getInvokeExpr().isInstanceInvokeExpr()) {
+            if (statement.getInvokeExpr().getBase().toString().equals(dataFlowVal.toString())) {
+                return BackwardQuery.make(dataFlowEdge, statement.getInvokeExpr().getBase());
+            }
+        }
+
+        return null;
+    }
+
+    /**
      * This method checks whether the current statement contains the sink method call. If yes, it return true.
      *
      * @param statement    Current Statement
@@ -72,36 +110,35 @@ public class BoomerangGPHandler implements IDemandDrivenGuidedManager {
      * @return True is there is a sink method call and TaintFlow exist.
      */
     private BackwardQuery isSink(Statement statement, ControlFlowGraph.Edge dataFlowEdge, Val dataFlowVal) {
-        for (Method sinkMethod : singleFlow.getTo()) {
-            String sinkSootSignature = Utility.wrapInAngularBrackets(sinkMethod.getSignature());
+        if (statement.containsInvokeExpr()) {
+            for (Method sinkMethod : singleFlow.getTo()) {
+                String sinkSootSignature = Utility.wrapInAngularBrackets(sinkMethod.getSignature());
 
-            if (statement.containsInvokeExpr() &&
-                    Utility.toStringEquals(statement.getInvokeExpr().getMethod().getSignature(),
-                            sinkSootSignature)) {   // If there is a sink method call, then check is there a taintflow present
+                // Normal check
+                if (Utility.toStringEquals(statement.getInvokeExpr().getMethod().getSignature(), sinkSootSignature)) {
+                    // If there is a sink method call, then check is there a taintflow present
 
-                //For sinks there is always a InFlow, there is no OutFlow.
-
-                // Check for the InFlow parameters
-                if (sinkMethod.getInputParameters() != null) {
-                    for (InputParameter input : sinkMethod.getInputParameters()) {
-                        int parameterIndex = input.getParamID();
-                        if (statement.getInvokeExpr().getArgs().size() >= parameterIndex) {
-                            if (statement.getInvokeExpr().getArg(parameterIndex).toString().equals(dataFlowVal.toString())) {
-                                return BackwardQuery.make(dataFlowEdge, statement.getInvokeExpr().getArg(parameterIndex));
-                            }
-                        }
-                    }
+                    return getBackWardQueriesBasedOnRules(sinkMethod, statement, dataFlowEdge, dataFlowVal);
                 }
 
-                // Check for InFlow this-object
-                if (sinkMethod.isInputThis() &&
-                        statement.getInvokeExpr().isInstanceInvokeExpr()) {
-                    if (statement.getInvokeExpr().getBase().toString().equals(dataFlowVal.toString())) {
-                        return BackwardQuery.make(dataFlowEdge, statement.getInvokeExpr().getBase());
-                    }
+                // Default value constructor check
+                if (isDefaultConstructor(sinkSootSignature, statement.getInvokeExpr().getMethod().getSignature())) {
+                    return getBackWardQueriesBasedOnRules(sinkMethod, statement, dataFlowEdge, dataFlowVal);
                 }
+
+                // Top level Default value function check
+                if (isTopLevelDefaultFunction(sinkSootSignature, statement.getInvokeExpr().getMethod().getSignature())) {
+                    return getBackWardQueriesBasedOnRules(sinkMethod, statement, dataFlowEdge, dataFlowVal);
+                }
+
+                // Class level Default value function check
+                Method modifiedSinkMethod = isClassLevelDefaultFunction(sinkMethod, statement.getInvokeExpr().getMethod().getSignature());
+
+                if (modifiedSinkMethod != null) {
+                    return getBackWardQueriesBasedOnRules(modifiedSinkMethod, statement, dataFlowEdge, dataFlowVal);
+                }
+
             }
-
         }
 
         return null;
@@ -200,6 +237,135 @@ public class BoomerangGPHandler implements IDemandDrivenGuidedManager {
     }
 
     /**
+     * This method checks whether the method signature is a default value constructor in Kotlin
+     *
+     * @param originalMethod Original method signature given in the fluentTQL specification
+     * @param sootSignature  Soot method signature obtained by the Statement
+     * @return Returns true if sootSignature method signature is the default value constructor version of originalMethod
+     */
+    private static boolean isDefaultConstructor(String originalMethod, String sootSignature) {
+        if (!sootSignature.contains("<init>(")) {
+            return false;
+        }
+
+        if (!originalMethod.contains("<init>(")) {
+            return false;
+        }
+
+        String[] temp = originalMethod.split("\\)");
+
+        if (temp.length != 2)
+            return false;
+
+        String processedOriginalMethod = temp[0] + ",int,kotlin.jvm.internal.DefaultConstructorMarker)>";
+
+        return Utility.toStringEquals(processedOriginalMethod, sootSignature);
+    }
+
+    /**
+     * This method checks whether the method signature is a top level default value function in Kotlin
+     *
+     * @param originalMethod Original method signature given in the fluentTQL specification
+     * @param sootSignature  Soot method signature obtained by the Statement
+     * @return Returns true if sootSignature method signature is the top level default value function version of originalMethod
+     */
+    private static boolean isTopLevelDefaultFunction(String originalMethod, String sootSignature) {
+        String[] temp1 = originalMethod.split("\\(");
+
+        if (temp1.length != 2)
+            return false;
+
+        String[] temp2 = temp1[1].split("\\)");
+
+        if (temp2.length != 2)
+            return false;
+
+        String processedOriginalMethod = temp1[0] + "$default(" + temp2[0] + ",int,java.lang.Object)>";
+
+        return Utility.toStringEquals(processedOriginalMethod, sootSignature);
+    }
+
+    /**
+     * This method checks whether the method signature is a class level default value function in Kotlin
+     *
+     * @param originalMethod Original method signature given in the fluentTQL specification
+     * @param sootSignature  Soot method signature obtained by the Statement
+     * @return Returns the modified Method if it is class level default value function, if not it returns null
+     */
+    private static Method isClassLevelDefaultFunction(Method originalMethod, String sootSignature) {
+        String[] temp1 = originalMethod.getSignature().split("\\(");
+
+        if (temp1.length != 2)
+            return null;
+
+        String[] temp3 = temp1[0].split(":");
+
+        if (temp3.length != 2)
+            return null;
+
+        String className = temp3[0].substring(1);
+
+        String[] temp2 = temp1[1].split("\\)");
+
+        if (temp2.length != 2)
+            return null;
+
+        String processedOriginalMethod = temp1[0] + "$default(" + className + "," + temp2[0] + ",int,java.lang.Object)>";
+
+        if (Utility.toStringEquals(processedOriginalMethod, sootSignature)) {
+            MethodImpl method = new MethodImpl();
+
+            // Signature
+            method.setSignature(processedOriginalMethod);
+
+            // Output return value
+            method.setReturnValue(originalMethod.getReturnValue());
+
+            List<InputParameter> inputParameters = new ArrayList<>();
+            List<OutputParameter> outputParameters = new ArrayList<>();
+
+            // this object
+            method.setInputThis(false);
+            method.setOutputThis(false);
+
+            if (originalMethod.isInputThis()) {
+                InputParameter inputParameter = new InputParameter();
+                inputParameter.setParamID(0);
+
+                inputParameters.add(inputParameter);
+            }
+
+            if (originalMethod.isOutputThis()) {
+                OutputParameter outputParameter = new OutputParameter();
+                outputParameter.setParamID(0);
+
+                outputParameters.add(outputParameter);
+            }
+
+            // Parameters
+            for (InputParameter inputParameter : originalMethod.getInputParameters()) {
+                InputParameter temp = new InputParameter();
+                temp.setParamID(inputParameter.getParamID() + 1);
+                inputParameters.add(temp);
+            }
+
+            for (OutputParameter outputParameter : originalMethod.getOutputParameters()) {
+                OutputParameter temp = new OutputParameter();
+                temp.setParamID(outputParameter.getParamID() + 1);
+                outputParameters.add(temp);
+            }
+
+            method.setOutputParameters(outputParameters);
+            method.setInputParameters(inputParameters);
+            method.setName(originalMethod.getName());
+
+            return method;
+        }
+
+        return null;
+    }
+
+    /**
      * This method check whether the current statement contains a propagator.
      *
      * @param propogators  List of propagators from the specification
@@ -211,20 +377,51 @@ public class BoomerangGPHandler implements IDemandDrivenGuidedManager {
     private Collection<Query> isPropogator(List<MethodImpl> propogators, Statement statement, ControlFlowGraph.Edge dataFlowEdge, Val dataFlowVal) {
         List<Query> queryList = new ArrayList<>();
 
-        for (Method requiredPropogatorMethod : propogators) {
-            String requiredPropogatorSootSignature = Utility.wrapInAngularBrackets(requiredPropogatorMethod.getSignature());
+        if (statement.containsInvokeExpr()) {
+            for (Method requiredPropogatorMethod : propogators) {
+                String requiredPropogatorSootSignature = Utility.wrapInAngularBrackets(requiredPropogatorMethod.getSignature());
 
-            if (statement.containsInvokeExpr() &&
-                    Utility.toStringEquals(statement.getInvokeExpr().getMethod().getSignature(),
-                            requiredPropogatorSootSignature)) { // If there is a propagator, then check is there a tainted variable going into the method
+                // Normal check
+                if (Utility.toStringEquals(statement.getInvokeExpr().getMethod().getSignature(), requiredPropogatorSootSignature)) {
+                    // If there is a propagator, then check is there a tainted variable going into the method
 
-                queryList.addAll(getQueriesBasedOnTheRules(requiredPropogatorMethod, statement, dataFlowEdge, dataFlowVal));
+                    queryList.addAll(getQueriesBasedOnTheRules(requiredPropogatorMethod, statement, dataFlowEdge, dataFlowVal));
 
-            } else if (statement.getInvokeExpr().getMethod().getSubSignature().equals(requiredPropogatorMethod.getSignature())) { // For Java 11
-                // Todo: Check and model it correctly to handle the Java 11 dynamic calls.
-                queryList.addAll(getQueriesBasedOnTheRules(requiredPropogatorMethod, statement, dataFlowEdge, dataFlowVal));
+                    return queryList;
+                }
+
+                // Check for Java 11 concat methods
+                if (statement.getInvokeExpr().getMethod().getSubSignature().equals(requiredPropogatorMethod.getSignature())) { // For Java 11
+                    // Todo: Check and model it correctly to handle the Java 11 dynamic calls.
+                    queryList.addAll(getQueriesBasedOnTheRules(requiredPropogatorMethod, statement, dataFlowEdge, dataFlowVal));
+
+                    return queryList;
+                }
+
+                // Default value constructor check
+                if (isDefaultConstructor(requiredPropogatorSootSignature, statement.getInvokeExpr().getMethod().getSignature())) {
+                    queryList.addAll(getQueriesBasedOnTheRules(requiredPropogatorMethod, statement, dataFlowEdge, dataFlowVal));
+
+                    return queryList;
+                }
+
+                // Top level Default value function check
+                if (isTopLevelDefaultFunction(requiredPropogatorSootSignature, statement.getInvokeExpr().getMethod().getSignature())) {
+                    queryList.addAll(getQueriesBasedOnTheRules(requiredPropogatorMethod, statement, dataFlowEdge, dataFlowVal));
+
+                    return queryList;
+                }
+
+                // Class level Default value function check
+                Method modifiedRequiredPropogatorMethod = isClassLevelDefaultFunction(requiredPropogatorMethod, statement.getInvokeExpr().getMethod().getSignature());
+
+                if (modifiedRequiredPropogatorMethod != null) {
+                    queryList.addAll(getQueriesBasedOnTheRules(modifiedRequiredPropogatorMethod, statement, dataFlowEdge, dataFlowVal));
+
+                    return queryList;
+                }
+
             }
-
         }
 
         return queryList;
